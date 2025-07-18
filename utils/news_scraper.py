16eedd6436e1eb8a2f576a8d0f21a3cb0e8a27b9
Diegoddm <ddmmoreira@gmail.com>
2025-07-18 16:57:45 -0400

    Add files via upload

--- utils/news_scraper.py
+++ utils/news_scraper.py
@@ -1,12 +1,1 @@
-import feedparser
-
-def get_news():
-    feed = feedparser.parse("https://news.google.com/rss/search?q=dólar+OR+brasil+economia+OR+usd+brl&hl=pt-BR&gl=BR&ceid=BR:pt-419")
-    news = []
-    for entry in feed.entries[:10]:
-        news.append({
-            "title": entry.title,
-            "link": entry.link,
-            "published": entry.published
-        })
-    return news
+# news scraper script
--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,6 +1,1 @@
-import yfinance as yf
-import pandas as pd
-
-def get_hourly_data():
-    df = yf.download("USDBRL=X", interval="60m", period="7d")
-    return df
+# preprocess script
